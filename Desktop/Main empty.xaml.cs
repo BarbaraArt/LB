cf29de9f42acf3d79aba0f95b31418950ee961e2
Варвара Фомина <justshootme.ak47@gmail.com>
2025-10-14 21:55:49 +0300

    создали окно main empty

--- Desktop/Main empty.xaml.cs
+++ Desktop/Main empty.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Desktop
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Main_empty.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Main_empty : Window
     {
-        public MainWindow()
+        public Main_empty()
         {
             InitializeComponent();
         }
     }
-    
 }
